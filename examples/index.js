250983cec6b6f68463422bb3bd48170f94a07312
jesseokeya <jesseokeya@gmail.com>
2019-03-13 10:56:26 -0400

    changes

--- examples/index.js
+++ examples/index.js
@@ -1,29 +1,39 @@
 const forbes = require('../')
 
-/**  Retrieves all billionaires **/
+/**  
+ * Retrieves all billionaires 
+ */
 forbes.list().then(response => {
     console.log(response)
 })
 
-/**  Limits the length of the returning array **/
+/**  
+ * Limits the length of the returning array 
+ */
 forbes.list({ limit: 5 }).then(response => {
     console.log(response)
 })
 
-/**  Limits the length of the returning array **/
-/** Valid parameters for filter -> youngest, oldest, women or female, men or male, real-time **/
+/**  
+ * Limits the length of the returning array
+ * Valid parameters for filter -> youngest, oldest, women or female, men or male, real-time 
+ */
 forbes.list({ limit: 5, filter: 'real-time' }).then(response => {
     console.log(response)
 })
 
-/**  Limits the length of the returning array **/
-/** Any valid state in the usa  **/
+/** 
+ * Limits the length of the returning array
+ * Any valid state in the usa  
+ */
 forbes.list({ limit: 5, filter: { state: 'texas' } }).then(response => {
     console.log(response)
 })
 
-/**  Limits the length of the returning array **/
-/** Valid industries include -> technology, fashion, finance, investments etc.. **/
+/**  
+ * Limits the length of the returning array
+ *  Valid industries include -> technology, fashion, finance, investments etc.. 
+ */
 forbes.list({ limit: 5, filter: { industry: 'fashion' } }).then(response => {
     console.log(response)
 })
